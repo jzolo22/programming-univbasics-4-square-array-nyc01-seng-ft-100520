1c209e31ba460ed481deb6aef523344f195e5c5e
Julia Zolotarev <jzolotarev@gmail.com>
2020-09-08 18:52:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,6 +3,7 @@ def square_array(array)
   counter = 0
   length = array.length
   while counter < array.length do
-    new_array.push(array[counter])
+    new_array.push(array[counter]^2)
+    counter += 1
   end
 end
